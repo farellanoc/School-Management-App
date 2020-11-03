f57020d6461569dca21a37d325c5b9166a69a174
cristianvia <cristianviar@gmail.com>
2020-11-03 09:37:00 +0100

    Add registerAdmin function

--- functions.php
+++ functions.php
@@ -98,6 +98,64 @@ function register()
     }
 }
 
+// REGISTER USER
+function registerAdmin()
+{
+    // call these variables with the global keyword to make them available in function
+    global $db, $errors, $username, $email;
+
+    // receive all input values from the form. Call the e() function
+    // defined below to escape form values
+    $username = e($_POST['username']);
+    $name = e($_POST['name']);
+    $email = e($_POST['email']);
+    $password_1 = e($_POST['password_1']);
+    $password_2 = e($_POST['password_2']);
+
+    // form validation: ensure that the form is correctly filled
+    if (empty($username)) {
+        array_push($errors, "Se requiere un nombre de usuario");
+    }
+    if (empty($email)) {
+        array_push($errors, "Se requiere un email");
+    }
+    if (empty($password_1)) {
+        array_push($errors, "Se requiere una clave");
+    }
+    if ($password_1 != $password_2) {
+        array_push($errors, "Las claves no coinciden");
+    }
+    //comprueba que no exista el usuario
+    $check = "SELECT username FROM users_admin WHERE username = '$username'";
+    $res_check = mysqli_query($db, $check);
+    if (mysqli_num_rows($res_check) >= 1) {
+        array_push($errors, "El usuario ya existe");
+    }
+    //comprueba que no exista el correo
+    $check = "SELECT email FROM users_admin WHERE email = '$email'";
+    $res_check = mysqli_query($db, $check);
+    if (mysqli_num_rows($res_check) >= 1) {
+        array_push($errors, "El correo ya existe");
+    }
+
+    // register user if there are no errors in the form
+    if (count($errors) == 0) {
+        $password = md5($password_1); //encrypt the password before saving in the database
+        $check = "SELECT * FROM users_admin WHERE username = '$username'";
+        $res_check = mysqli_query($db, $check);
+        if (mysqli_num_rows($res_check) == 0) {
+            $query = "INSERT INTO users_admin (id_user_admin, username, name, email, password) 
+					  VALUES(NULL, '$username', '$name', '$email', '$password')";
+            mysqli_query($db, $query);
+        }
+        // get id of the created user
+        $logged_in_user_id = mysqli_insert_id($db);
+
+        $_SESSION['admin'] = getUserById($logged_in_user_id); // put logged in user in session
+        $_SESSION['success'] = "Administrador creado correctamente";
+    }
+}
+
 //Update function
 
 function updateUser()
